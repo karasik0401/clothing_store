8a33ce1818464ea0853075a9e3c0a339c00bcd92
karasik0401 <85022375+karasik0401@users.noreply.github.com>
2023-12-13 14:36:40 +0300

    Update settings.py

--- cart/app/settings.py
+++ cart/app/settings.py
@@ -1,6 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    postgres_url: str = "postgresql://postgres:password@microservice-postgres-1:5432/cart"
+    postgres_url: str
+    
+    model_config = SettingsConfigDict(env_file='.env')
 
 settings = Settings()
